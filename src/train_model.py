0b69059bfe6d72c50b6bac23e7e2cc9aeb1a9399
Ana Ixchel Pérez Amezcua <ana.ixchel98@gmail.com>
2026-03-13 12:46:41 -0600

    Create train_model.py

---
+++ src/train_model.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from load_data import load_eeg_data
+from preprocess import preprocess_data
+
+data = load_eeg_data("../data/eeg_sample_data.csv")
+
+X, y = preprocess_data(data)
+
+model = RandomForestClassifier()
+
+model.fit(X, y)
+
+print("Model trained successfully")
